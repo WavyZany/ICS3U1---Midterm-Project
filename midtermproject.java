e69bac14c7b28a061861a03218a7d0f3a3571887
WavyZany <130682641+WavyZany@users.noreply.github.com>
2023-04-19 00:39:47 -0400

    Scene 2
    Scene 2 was created along with animation
    Scene 3 was started
    Additional changes like text colour

--- midtermproject.java
+++ midtermproject.java
@@ -1,6 +1,7 @@
 import arc.*;
 import java.awt.image.BufferedImage;
-//  test
+import java.awt.Color;
+
 
 
 
@@ -17,19 +18,19 @@ public class midtermproject{
 		String strInitial;
 		
 		con.println("SEARGEANT MILLAR: Here is a quick update young adventurer");
-		con.sleep(5000);
+		//con.sleep(5000);
 		con.clear();
 		con.println("SEARGEANT MILLAR: Mythical monsters from the universe Cestearus are attacking our clan");
-		con.sleep(5000);
+		//con.sleep(5000);
 		con.clear();
 		con.println("SEARGEANT MILLAR: It is up to YOU to destroy these monsters and bring peace");
-		con.sleep(5000);
+		//con.sleep(5000);
 		con.clear();
 		con.println("SEARGEANT MILLAR: Give me your name young traveler");
 		strName = con.readLine();
 		con.clear();
 		con.println(strName+" is a noble name");
-		con.sleep(5000);
+		//con.sleep(5000);
 		con.clear();
 		
 		strInitial = "";
@@ -38,8 +39,11 @@ public class midtermproject{
 		
 		if(strInitial.equals("a") || strInitial.equals("e") || strInitial.equals("i") || strInitial.equals("o") || strInitial.equals("u")){
 			// Scene 2
-			
+			con.setTextColor(Color.BLACK);
 			con.println("Good luck WARRIOR "+strName);
+			
+			scene2(con);
+
 		}else if(strInitial.equals(strInitial.toUpperCase())){
 			// Scene 4
 			
@@ -48,6 +52,8 @@ public class midtermproject{
 			// Scene 3
 			
 			con.println("Good luck MAGE "+strName);
+			
+			scene3(con);
 		}
 		
 		
@@ -65,5 +71,66 @@ public class midtermproject{
 		con.repaint();
 	}
 	
+	// Scene 2 - You are welcomed as a melee WARRIOR Flaming Sword Animation
+	public static void scene2(Console con){
+		BufferedImage imgScene2;
+		imgScene2 = con.loadImage("BFSscene2.jpg");
+		
+		//con.repaint();
+		
+		int intCounter;
+		// Code is glitching out for some reason
+		for(intCounter = 500; intCounter > 200; intCounter--){
+			con.setDrawColor(new Color(133, 42, 42));
+			con.fillOval(250,intCounter,30,30);
+			con.drawImage(imgScene2,0,0);
+			con.repaint();
+		}
+		for(intCounter = 500; intCounter > 200; intCounter--){	
+			con.setDrawColor(new Color(133, 42, 42));
+			con.fillOval(400,intCounter,30,30);
+			con.drawImage(imgScene2,0,0);
+			con.repaint();
+		}
+		for(intCounter = 500; intCounter > 200; intCounter--){
+			con.setDrawColor(new Color(133, 42, 42));
+			con.fillOval(250,intCounter- 50,30,30);
+			con.drawImage(imgScene2,0,0);
+			con.repaint();
+		}
+		for(intCounter = 500; intCounter > 200; intCounter--){
+			con.setDrawColor(new Color(133, 42, 42));
+			con.fillOval(300,intCounter - 75,30,30);
+			con.drawImage(imgScene2,0,0);
+			con.repaint();
+		}
+		for(intCounter = 500; intCounter > 200; intCounter--){
+			con.setDrawColor(new Color(133, 42, 42));
+			con.fillOval(350,intCounter - 100,30,30);
+			con.drawImage(imgScene2,0,0);
+			con.repaint();
+		}
+		for(intCounter = 500; intCounter > 200; intCounter--){
+			con.setDrawColor(new Color(133, 42, 42));
+			con.fillOval(200,intCounter + 100,30,30);
+			con.drawImage(imgScene2,0,0);
+			con.repaint();
+		}
+	}
+		
+	// Scene 3 - You are welcomed as a magic wielding MAGE Flaming Staff Animation
+
+	public static void scene3(Console con){
+		BufferedImage = imgScene3;
+		imgScene3 = con.loadImage("BFSscene3.jpg");
+		
+		con.drawImage(imgScene3,0,0);
+		con.repaint();
+	}
+		
+		
+		
+
+	}
 	
 } // "public class midtermproject" ENDING bracket
